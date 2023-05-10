354490e92d718830f4cfdd1f572bd82371b064cf
Kyhl <kasper.sebastian@gmail.com>
2023-05-10 19:16:13 +0200

    Bomba

--- exercises/exercise08/SceneViewerApplication.cpp
+++ exercises/exercise08/SceneViewerApplication.cpp
@@ -54,7 +54,7 @@ void SceneViewerApplication::Initialize()
     m_imGui.Initialize(GetMainWindow());
 
     InitializeCamera();
-    InitializeLights();
+    //InitializeLights();
     InitializeMaterial();
     InitializeModels();
     InitializeRenderer();
@@ -113,9 +113,9 @@ void SceneViewerApplication::InitializeCamera()
 void SceneViewerApplication::InitializeLights()
 {
     // Create a directional light and add it to the scene
-    std::shared_ptr<DirectionalLight> directionalLight = std::make_shared<DirectionalLight>();
-    directionalLight->SetDirection(glm::vec3(-0.3f, -1.0f, -0.3f)); // It will be normalized inside the function
-    directionalLight->SetIntensity(3.0f);
+    //std::shared_ptr<DirectionalLight> directionalLight = std::make_shared<DirectionalLight>();
+    //directionalLight->SetDirection(glm::vec3(-0.3f, -1.0f, -0.3f)); // It will be normalized inside the function
+    //directionalLight->SetIntensity(3.0f);
     //m_scene.AddSceneNode(std::make_shared<SceneLight>("directional light", directionalLight));
 
     // Create a point light and add it to the scene
@@ -127,56 +127,56 @@ void SceneViewerApplication::InitializeLights()
 
 void SceneViewerApplication::InitializeMaterial()
 {
-    // Load and build shader
-    std::vector<const char*> vertexShaderPaths;
-    vertexShaderPaths.push_back("shaders/version330.glsl");
-    vertexShaderPaths.push_back("shaders/default.vert");
-    Shader vertexShader = ShaderLoader(Shader::VertexShader).Load(vertexShaderPaths);
+    //// Load and build shader
+    //std::vector<const char*> vertexShaderPaths;
+    //vertexShaderPaths.push_back("shaders/version330.glsl");
+    //vertexShaderPaths.push_back("shaders/default.vert");
+    //Shader vertexShader = ShaderLoader(Shader::VertexShader).Load(vertexShaderPaths);
 
-    std::vector<const char*> fragmentShaderPaths;
-    fragmentShaderPaths.push_back("shaders/version330.glsl");
-    fragmentShaderPaths.push_back("shaders/utils.glsl");
-    fragmentShaderPaths.push_back("shaders/lambert-ggx.glsl");
-    fragmentShaderPaths.push_back("shaders/lighting.glsl");
-    fragmentShaderPaths.push_back("shaders/default_pbr.frag");
-    Shader fragmentShader = ShaderLoader(Shader::FragmentShader).Load(fragmentShaderPaths);
+    //std::vector<const char*> fragmentShaderPaths;
+    //fragmentShaderPaths.push_back("shaders/version330.glsl");
+    //fragmentShaderPaths.push_back("shaders/utils.glsl");
+    //fragmentShaderPaths.push_back("shaders/lambert-ggx.glsl");
+    //fragmentShaderPaths.push_back("shaders/lighting.glsl");
+    //fragmentShaderPaths.push_back("shaders/default_pbr.frag");
+    //Shader fragmentShader = ShaderLoader(Shader::FragmentShader).Load(fragmentShaderPaths);
 
-    std::shared_ptr<ShaderProgram> shaderProgramPtr = std::make_shared<ShaderProgram>();
-    shaderProgramPtr->Build(vertexShader, fragmentShader);
+    //std::shared_ptr<ShaderProgram> shaderProgramPtr = std::make_shared<ShaderProgram>();
+    //shaderProgramPtr->Build(vertexShader, fragmentShader);
 
-    // Get transform related uniform locations
-    ShaderProgram::Location cameraPositionLocation = shaderProgramPtr->GetUniformLocation("CameraPosition");
-    ShaderProgram::Location worldMatrixLocation = shaderProgramPtr->GetUniformLocation("WorldMatrix");
-    ShaderProgram::Location viewProjMatrixLocation = shaderProgramPtr->GetUniformLocation("ViewProjMatrix");
+    //// Get transform related uniform locations
+    //ShaderProgram::Location cameraPositionLocation = shaderProgramPtr->GetUniformLocation("CameraPosition");
+    //ShaderProgram::Location worldMatrixLocation = shaderProgramPtr->GetUniformLocation("WorldMatrix");
+    //ShaderProgram::Location viewProjMatrixLocation = shaderProgramPtr->GetUniformLocation("ViewProjMatrix");
 
-    // Register shader with renderer
-    m_renderer.RegisterShaderProgram(shaderProgramPtr,
-        [=](const ShaderProgram& shaderProgram, const glm::mat4& worldMatrix, const Camera& camera, bool cameraChanged)
-        {
-            if (cameraChanged)
-            {
-                shaderProgram.SetUniform(cameraPositionLocation, camera.ExtractTranslation());
-                shaderProgram.SetUniform(viewProjMatrixLocation, camera.GetViewProjectionMatrix());
-            }
-            shaderProgram.SetUniform(worldMatrixLocation, worldMatrix);
-        },
-        m_renderer.GetDefaultUpdateLightsFunction(*shaderProgramPtr)
-    );
+    //// Register shader with renderer
+    //m_renderer.RegisterShaderProgram(shaderProgramPtr,
+    //    [=](const ShaderProgram& shaderProgram, const glm::mat4& worldMatrix, const Camera& camera, bool cameraChanged)
+    //    {
+    //        if (cameraChanged)
+    //        {
+    //            shaderProgram.SetUniform(cameraPositionLocation, camera.ExtractTranslation());
+    //            shaderProgram.SetUniform(viewProjMatrixLocation, camera.GetViewProjectionMatrix());
+    //        }
+    //        shaderProgram.SetUniform(worldMatrixLocation, worldMatrix);
+    //    },
+    //    m_renderer.GetDefaultUpdateLightsFunction(*shaderProgramPtr)
+    //);
 
-    // Filter out uniforms that are not material properties
-    ShaderUniformCollection::NameSet filteredUniforms;
-    filteredUniforms.insert("CameraPosition");
-    filteredUniforms.insert("WorldMatrix");
-    filteredUniforms.insert("ViewProjMatrix");
-    filteredUniforms.insert("LightIndirect");
-    filteredUniforms.insert("LightColor");
-    filteredUniforms.insert("LightPosition");
-    filteredUniforms.insert("LightDirection");
-    filteredUniforms.insert("LightAttenuation");
+    //// Filter out uniforms that are not material properties
+    //ShaderUniformCollection::NameSet filteredUniforms;
+    //filteredUniforms.insert("CameraPosition");
+    //filteredUniforms.insert("WorldMatrix");
+    //filteredUniforms.insert("ViewProjMatrix");
+    //filteredUniforms.insert("LightIndirect");
+    //filteredUniforms.insert("LightColor");
+    //filteredUniforms.insert("LightPosition");
+    //filteredUniforms.insert("LightDirection");
+    //filteredUniforms.insert("LightAttenuation");
 
-    // Create reference material
-    assert(shaderProgramPtr);
-    m_defaultMaterial = std::make_shared<Material>(shaderProgramPtr, filteredUniforms);
+    //// Create reference material
+    //assert(shaderProgramPtr);
+    //m_defaultMaterial = std::make_shared<Material>(shaderProgramPtr, filteredUniforms);
 
     
     //Shader vertexShader(Shader::VertexShader);
@@ -246,33 +246,33 @@ void SceneViewerApplication::InitializeModels()
     m_skyboxTexture->GetParameter(TextureObject::ParameterFloat::MaxLod, maxLod);
     TextureCubemapObject::Unbind();
 
-    m_defaultMaterial->SetUniformValue("AmbientColor", glm::vec3(0.25f));
+    //m_defaultMaterial->SetUniformValue("AmbientColor", glm::vec3(0.25f));
 
-    m_defaultMaterial->SetUniformValue("EnvironmentTexture", m_skyboxTexture);
-    m_defaultMaterial->SetUniformValue("EnvironmentMaxLod", maxLod);
-    m_defaultMaterial->SetUniformValue("Color", glm::vec3(1.0f));
+    //m_defaultMaterial->SetUniformValue("EnvironmentTexture", m_skyboxTexture);
+    //m_defaultMaterial->SetUniformValue("EnvironmentMaxLod", maxLod);
+    //m_defaultMaterial->SetUniformValue("Color", glm::vec3(1.0f));
 
-    // Configure loader
-    ModelLoader loader(m_defaultMaterial);
+    //// Configure loader
+    //ModelLoader loader(m_defaultMaterial);
 
-    // Create a new material copy for each submaterial
-    loader.SetCreateMaterials(true);
+    //// Create a new material copy for each submaterial
+    //loader.SetCreateMaterials(true);
 
-    // Flip vertically textures loaded by the model loader
-    loader.GetTexture2DLoader().SetFlipVertical(true);
+    //// Flip vertically textures loaded by the model loader
+    //loader.GetTexture2DLoader().SetFlipVertical(true);
 
-    // Link vertex properties to attributes
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Position, "VertexPosition");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Normal, "VertexNormal");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Tangent, "VertexTangent");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::Bitangent, "VertexBitangent");
-    loader.SetMaterialAttribute(VertexAttribute::Semantic::TexCoord0, "VertexTexCoord");
+    //// Link vertex properties to attributes
+    //loader.SetMaterialAttribute(VertexAttribute::Semantic::Position, "VertexPosition");
+    //loader.SetMaterialAttribute(VertexAttribute::Semantic::Normal, "VertexNormal");
+    //loader.SetMaterialAttribute(VertexAttribute::Semantic::Tangent, "VertexTangent");
+    //loader.SetMaterialAttribute(VertexAttribute::Semantic::Bitangent, "VertexBitangent");
+    //loader.SetMaterialAttribute(VertexAttribute::Semantic::TexCoord0, "VertexTexCoord");
 
-    // Link material properties to uniforms
-    loader.SetMaterialProperty(ModelLoader::MaterialProperty::DiffuseColor, "Color");
-    loader.SetMaterialProperty(ModelLoader::MaterialProperty::DiffuseTexture, "ColorTexture");
-    loader.SetMaterialProperty(ModelLoader::MaterialProperty::NormalTexture, "NormalTexture");
-    loader.SetMaterialProperty(ModelLoader::MaterialProperty::SpecularTexture, "SpecularTexture");
+    //// Link material properties to uniforms
+    //loader.SetMaterialProperty(ModelLoader::MaterialProperty::DiffuseColor, "Color");
+    //loader.SetMaterialProperty(ModelLoader::MaterialProperty::DiffuseTexture, "ColorTexture");
+    //loader.SetMaterialProperty(ModelLoader::MaterialProperty::NormalTexture, "NormalTexture");
+    //loader.SetMaterialProperty(ModelLoader::MaterialProperty::SpecularTexture, "SpecularTexture");
 
     // Load models
     //std::shared_ptr<Model> chestModel = loader.LoadShared("models/treasure_chest/treasure_chest.obj");
@@ -303,7 +303,7 @@ void SceneViewerApplication::InitializeModels()
 
     // Create container for the element data
     std::vector<unsigned int> indices;
-    unsigned int m_grid = 256u;
+    unsigned int m_grid = 512u;
     // Grid scale to convert the entire grid to size 1x1
     float scale = (1.0f / m_grid);
 
@@ -461,10 +461,10 @@ void SceneViewerApplication::InitializeModels()
 
                 vec3 deltaX = normalize(vertices[nextX].position - vertices[prevX].position);
                 vec3 deltaY = normalize(vertices[nextY].position - vertices[prevY].position);
-                vec3 temp = cross(deltaY, deltaX);
+                vec3 temp = cross(deltaX, deltaY);
                 if (sign(dot(vertex.normal, temp)) > 0)
                 {
-                    vertex.normal = cross(deltaX, deltaY);
+                    vertex.normal = cross(deltaY, deltaX);
                 }
                 else {
                     vertex.normal = temp;
@@ -478,8 +478,8 @@ void SceneViewerApplication::InitializeModels()
     planetModel->AddMaterial(m_planetMaterial);
     planetModel->GetMesh().AddSubmesh<Vertex, unsigned int, VertexFormat::LayoutIterator>(Drawcall::Primitive::Triangles, vertices, indices,
         vertexFormat.LayoutBegin(static_cast<int>(vertices.size()), true /* interleaved */), vertexFormat.LayoutEnd());
-    //    std::shared_ptr<Material> planetMaterial = std::make_shared<Material>();
     
+
 
     //Sun
     std::vector<Vertex> sunVertices;
@@ -488,7 +488,6 @@ void SceneViewerApplication::InitializeModels()
     std::vector<unsigned int> sunIndices;
     // Iterate over each VERTEX
 
-    
     float sunScale = 1.0f;
 
     for (unsigned int u = 0; u < 6; u++)
